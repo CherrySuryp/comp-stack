8c59eb9767490c5cd0b6ac0b0309bece1c6ffb16
CherrySuryp <hackyourrself@proton.me>
2024-12-15 00:10:27 +0100

    Use config for DataLoader file path default

--- app/utils.py
+++ app/utils.py
@@ -3,6 +3,8 @@ from functools import wraps
 
 import pandas as pd
 
+from app.config import config
+
 
 def singleton(cls):
     instances = {}
@@ -18,7 +20,7 @@ def singleton(cls):
 
 @singleton
 class DataLoader:
-    def __init__(self, file_path: str = "sales_data.csv"):
+    def __init__(self, file_path: str = config.DATA_FILE_PATH):
         if not os.path.exists(file_path):
             raise FileNotFoundError(f"Couldn't open file {file_path}")
         self._file_path = file_path
