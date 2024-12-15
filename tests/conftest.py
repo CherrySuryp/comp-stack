311c2575044f7370331f5cb864d2dd0fd0d77d37
CherrySuryp <hackyourrself@proton.me>
2024-12-15 01:37:10 +0100

    Remove unused test fixture and redundant mock data file

--- tests/conftest.py
+++
@@ -1,9 +0,0 @@
-import os
-
-import pytest
-
-
-@pytest.fixture(scope="session", autouse=True)
-def override_env():
-    os.environ["DATA_FILE_PATH"] = "tests/mocks/data.csv"
-    assert os.environ.get("DATA_FILE_PATH") == "tests/mocks/data.csv"
