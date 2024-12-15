ce61ae53b6e9883b43854624cdce9f16886a1713
CherrySuryp <hackyourrself@proton.me>
2024-12-15 13:57:35 +0100

    Add field examples and adjust code style with noqa comments

--- app/schema.py
+++ app/schema.py
@@ -5,8 +5,8 @@ from pydantic import BaseModel, Field, model_validator
 
 
 class DateRange(BaseModel):
-    start_date: datetime.date
-    end_date: datetime.date
+    start_date: datetime.date = Field(examples=[datetime.date(2023, 1, 1)])
+    end_date: datetime.date = Field(examples=[datetime.date.today()])
 
     @model_validator(mode="after")
     def verify_start_date_le_end_date(self) -> Self:
@@ -17,12 +17,12 @@ class DateRange(BaseModel):
 
 class Filters(BaseModel):
     date_range: DateRange | None = None
-    category: list[str] = Field(default_factory=list)
-    product_ids: list[int] = Field(default_factory=list)
+    category: list[str] = Field(default_factory=list) # noqa
+    product_ids: list[int] = Field(default_factory=list) # noqa
 
 
 class GetSummary(BaseModel):
-    columns: list[str] = Field(default=["quantity_sold", "price_per_unit"])
+    columns: list[str] = Field(default=["quantity_sold", "price_per_unit"]) # noqa
     filters: Filters | None = Field(default=None)
 
 
